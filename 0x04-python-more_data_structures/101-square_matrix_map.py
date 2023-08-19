b346aae7fae6ce6dddf5ae8e783fb6e891d77424
ThamiBa <baladithami@gmail.com>
2023-08-19 01:54:41 +0100

    fix issues

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
-
 def square_matrix_map(matrix=[]):
-    """
-    The function that computes the square value of all intgs of a matrix using
-    """
     return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
-
 def multiply_list_map(my_list=[], number=0):
-    """
-    A fct that returns a list with all val multi by a nb without using any lps
-    """
     return (list(map((lambda i: i * number), my_list)))
