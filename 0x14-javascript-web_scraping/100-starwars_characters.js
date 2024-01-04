b6f24bb0efa53a71b542c6f5b82cb2e21414f185
ThamiBa <baladithami@gmail.com>
2024-01-04 01:40:36 +0100

    👨‍💻 JS_Web_scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
-const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, function (error, response, body) {
-  if (error) return console.error('error:', error);
-
-  const characters = JSON.parse(body).characters;
-  printCharacters(characters, 0);
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+req.get(url + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
 });
-
-const printCharacters = (characters, index) => {
-  if (index >= characters.length) return;
-
-  request(characters[index], (error, response, body) => {
-    if (!error) console.log(JSON.parse(body).name);
-
-    printCharacters(characters, index + 1);
-  });
-};
-
